4d4376fe182a267cde42532c56911d2dbd02dc27
firefly-g <gantian_1998@163.com>
2020-10-23 17:57:29 +0800

    Jtopo

---
+++ es6语法学习/构造函数及原型/es5_obj.js
@@ -0,0 +1,16 @@
+/*   es6 之前 利用构造函数创建对象
+创建对象的三种方式：
+1.对象字面量         var obj={}
+2.new object()       var obj = new Object();
+3.自定义构造函数      function Star(uname,age){
+                        this.uname=uname;
+                        this.age=age;
+                      say(){}
+                     }
+                     var ldh =new Star('华华'，18);
+
+*/
+
+
+
+
